a7e933961bf9df09f757e051350389800ecdd0c2
kanahia <kanahia@iimcb.gov.pl>
2023-10-03 13:55:16 +0200

    C C A

--- R/main.R
+++ R/main.R
@@ -2,30 +2,12 @@
 # generate results --------------------------------------------------------
 
 # URL <- "https://lubimyczytac.pl/ksiegozbior/xc5GvparjG"
-# URL <- "https://lubimyczytac.pl/ksiegozbior/XyeMyrJrGh"
-#
-# res <-
-#   run_libroScrapeR(URL = URL)
+
+# Example how to run the entire workflow
+URL <- "https://lubimyczytac.pl/ksiegozbior/XyeMyrJrGh"
+
+res <-
+  run_libroScrapeR(URL = URL)
 
 
-
-#test
-
-# all_css_id <-
-#   page_html %>%
-#     html_nodes(xpath = '//*[@id]') %>%
-#     html_text2()
-#
-# when_read_pos <-
-#   all_css_id %>%
-#     grepl(pattern = "Przeczytał:\n20")
-#
-# all_css_id[when_read_pos] %>%
-#   substring(first = 13, last = 22)
-#
-#
-# # data ale nie na wszystko:
-# page_html %>%
-#   html_elements(css = 'div.small') %>%
-#   html_text2()
 
